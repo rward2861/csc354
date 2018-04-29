f23dac122895fc7fe53dd7f42fbde1da7d36916c
Richie Ward <33726576+rward2861@users.noreply.github.com>
2018-04-29 18:13:25 -0400

    Create socket_communication.py

---
+++ socket_communication.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+/************************************************************/
+/* Author(s):         3JRS - Janeel, Jennie, Jess, Richie, Steph
+/* Major:             Information Technology
+/* Compiler:          Python 3.6                  
+/* Creation Date:     4/29/18
+/* Due Date:          12/12/18
+/* Course:            CSC354-020
+/* Professor Name:    Prof. Demarco
+/* Assignment:        Software Engineering Project (Prototype)
+/* Filename:          socket_communication.py
+/* Purpose:           To write a server that will communicate with a client (which will also be a server) via client/server communication
+/************************************************************/
+"""
+import socket
+import sys, os 
