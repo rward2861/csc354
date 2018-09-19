7b55e2eef15e4382c8f10fe7ff70fec5ee90b999
Richie Ward <33726576+rward2861@users.noreply.github.com>
2018-09-19 15:43:28 -0400

    Update facialrecognition.py

--- facialrecognition.py
+++ facialrecognition.py
@@ -251,17 +251,29 @@ def predict(test_img):
 print("Predicting images...")
 
 #load test images
-test_img1 = cv2.imread("test-data/test1.jpg")
-test_img2 = cv2.imread("test-data/test2.jpg")
+test_img1 = cv2.imread("test-data/fig1.jpg")
+test_img2 = cv2.imread("test-data/fig2.jpg")
+test_img3 = cv2.imread("test-data/fig3.jpg")
+test_img4 = cv2.imread("test-data/fig4.jpg")
+test_img5 = cv2.imread("test-data/fig5.jpg")
+test_img6 = cv2.imread("test-data/fig6.jpg")
+test_img7 = cv2.imread("test-data/fig7.jpg")
 
 #perform a prediction
 predicted_img1 = predict(test_img1)
 predicted_img2 = predict(test_img2)
+predicted_img3 = predict(test_img3)
+predicted_img4 = predict(test_img4)
+predicted_img5 = predict(test_img5)
+predicted_img6 = predict(test_img6)
+predicted_img7 = predict(test_img7)
 print("Prediction complete")
 
 #display both images
 cv2.imshow(subjects[1], cv2.resize(predicted_img1, (400, 500)))
 cv2.imshow(subjects[2], cv2.resize(predicted_img2, (400, 500)))
+cv2.imshow(subjects[3], cv2.resize(predicted_img3, (400, 500)))
+cv2.imshow(subjects[4], cv2.resize(predicted_img4, (400, 500)))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 cv2.waitKey(1)
