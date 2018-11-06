5e8fccb24104fcc48377e3365e6d5dbaeed6b2ee
Richie Ward <33726576+rward2861@users.noreply.github.com>
2018-11-06 10:56:30 -0500

    Update facialrecognition.py

--- facialrecognition.py
+++ facialrecognition.py
@@ -24,17 +24,22 @@
 import cv2
 import os 
 import numpy as np
+import time 
+import matplotlib.pyplot as plt
 
-subjects = ["", "Richie?", "Janeel", "Steph?"]
+#there is no label 0 in our training data so subject name for index/label 0 is empty
+subjects = ["", "Richie", "Janeel", "Enrique Iglesias"]
 
 
-def detect_face(img):
+
+#function to detect face using OpenCV
+def find_faces(img):
     #convert the test image to gray image as opencv face detector expects gray images
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     #load OpenCV face detector, I am using LBP which is fast
     #there is also a more accurate but slow Haar classifier
-    face_cascade = cv2.CascadeClassifier('opencv-files/lbpcascade_frontalface.xml')
+    face_cascade = cv2.CascadeClassifier('opencv/sources/data/lbpcascades/lbpcascade_frontalface.xml')
 
     #let's detect multiscale (some images may be closer to camera than others) images
     #result is a list of faces
@@ -51,12 +56,6 @@ def detect_face(img):
     #return only the face part of the image
     return gray[y:y+w, x:x+h], faces[0]
 
-
-# I am using OpenCV's **LBP face detector**. On _line 4_, I convert the image to grayscale because most operations in OpenCV are performed in gray scale, then on _line 8_ I load LBP face detector using `cv2.CascadeClassifier` class. After that on _line 12_ I use `cv2.CascadeClassifier` class' `detectMultiScale` method to detect all the faces in the image. on _line 20_, from detected faces I only pick the first face because in one image there will be only one face (under the assumption that there will be only one prominent face). As faces returned by `detectMultiScale` method are actually rectangles (x, y, width, height) and not actual faces images so we have to extract face image area from the main image. So on _line 23_ I extract face area from gray image and return both the face image area and face rectangle.
-# 
-# Now you have got a face detector and you know the 4 steps to prepare the data, so are you ready to code the prepare data step? Yes? So let's do it. 
-
-# In[4]:
 
 #this function will read all persons' training images, detect face from each image
 #and will return two lists of exactly same size, one list 
@@ -88,7 +87,7 @@ def prepare_training_data(data_folder_path):
         
         #build path of directory containin images for current subject subject
         #sample subject_dir_path = "training-data/s1"
-        subject_dir_path = data_folder_path + "/" + dir_name
+        subject_dir_path = (data_folder_path + "/" + dir_name)
         
         #get the images names that are inside the given subject directory
         subject_images_names = os.listdir(subject_dir_path)
@@ -104,21 +103,19 @@ def prepare_training_data(data_folder_path):
             
             #build image path
             #sample image path = training-data/s1/1.pgm
-            image_path = subject_dir_path + "/" + image_name
+            image_path = (subject_dir_path + "/" + image_name)
 
             #read image
             image = cv2.imread(image_path)
             
             #display an image window to show the image 
             cv2.imshow("Training on image...", cv2.resize(image, (400, 500)))
-            cv2.waitKey(100)
+            cv2.waitKey(10)
             
             #detect face
-            face, rect = detect_face(image)
-            
-            #------STEP-4--------
-            #for the purpose of this tutorial
-            #we will ignore faces that are not detected
+            face, rect = find_faces(image)
+
+            #ignore if no faces are present
             if face is not None:
                 #add face to list of faces
                 faces.append(face)
@@ -131,24 +128,6 @@ def prepare_training_data(data_folder_path):
     
     return faces, labels
 
-
-# I have defined a function that takes the path, where training subjects' folders are stored, as parameter. This function follows the same 4 prepare data substeps mentioned above. 
-# 
-# **(step-1)** On _line 8_ I am using `os.listdir` method to read names of all folders stored on path passed to function as parameter. On _line 10-13_ I am defining labels and faces vectors. 
-# 
-# **(step-2)** After that I traverse through all subjects' folder names and from each subject's folder name on _line 27_ I am extracting the label information. As folder names follow the `sLabel` naming convention so removing the  letter `s` from folder name will give us the label assigned to that subject. 
-# 
-# **(step-3)** On _line 34_, I read all the images names of of the current subject being traversed and on _line 39-66_ I traverse those images one by one. On _line 53-54_ I am using OpenCV's `imshow(window_title, image)` along with OpenCV's `waitKey(interval)` method to display the current image being traveresed. The `waitKey(interval)` method pauses the code flow for the given interval (milliseconds), I am using it with 100ms interval so that we can view the image window for 100ms. On _line 57_, I detect face from the current image being traversed. 
-# 
-# **(step-4)** On _line 62-66_, I add the detected face and label to their respective vectors.
-
-# But a function can't do anything unless we call it on some data that it has to prepare, right? Don't worry, I have got data of two beautiful and famous celebrities. I am sure you will recognize them!
-# 
-# ![training-data](visualization/tom-shahrukh.png)
-# 
-# Let's call this function on images of these beautiful celebrities to prepare data for training of our Face Recognizer. Below is a simple code to do that.
-
-# In[5]:
 
 #let's first prepare our training data
 #data will be in two lists of same size
@@ -163,20 +142,6 @@ print("Total faces: ", len(faces))
 print("Total labels: ", len(labels))
 
 
-# This was probably the boring part, right? Don't worry, the fun stuff is coming up next. It's time to train our own face recognizer so that once trained it can recognize new faces of the persons it was trained on. Read? Ok then let's train our face recognizer. 
-
-# ### Train Face Recognizer
-
-# As we know, OpenCV comes equipped with three face recognizers.
-# 
-# 1. EigenFace Recognizer: This can be created with `cv2.face.createEigenFaceRecognizer()`
-# 2. FisherFace Recognizer: This can be created with `cv2.face.createFisherFaceRecognizer()`
-# 3. Local Binary Patterns Histogram (LBPH): This can be created with `cv2.face.LBPHFisherFaceRecognizer()`
-# 
-# I am going to use LBPH face recognizer but you can use any face recognizer of your choice. No matter which of the OpenCV's face recognizer you use the code will remain the same. You just have to change one line, the face recognizer initialization line given below. 
-
-# In[6]:
-
 #create our LBPH face recognizer 
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 
@@ -186,26 +151,8 @@ face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 #or use FisherFaceRecognizer by replacing above line with 
 #face_recognizer = cv2.face.FisherFaceRecognizer_create()
 
-
-# Now that we have initialized our face recognizer and we also have prepared our training data, it's time to train the face recognizer. We will do that by calling the `train(faces-vector, labels-vector)` method of face recognizer. 
-
-# In[7]:
-
 #train our face recognizer of our training faces
 face_recognizer.train(faces, np.array(labels))
-
-
-# **Did you notice** that instead of passing `labels` vector directly to face recognizer I am first converting it to **numpy** array? This is because OpenCV expects labels vector to be a `numpy` array. 
-# 
-# Still not satisfied? Want to see some action? Next step is the real action, I promise! 
-
-# ### Prediction
-
-# Now comes my favorite part, the prediction part. This is where we actually get to see if our algorithm is actually recognizing our trained subjects's faces or not. We will take two test images of our celeberities, detect faces from each of them and then pass those faces to our trained face recognizer to see if it recognizes them. 
-# 
-# Below are some utility functions that we will use for drawing bounding box (rectangle) around face and putting celeberity name near the face bounding box. 
-
-# In[8]:
 
 #function to draw rectangle on image 
 #according to given (x, y) coordinates and 
@@ -220,14 +167,6 @@ def draw_text(img, text, x, y):
     cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
 
 
-# First function `draw_rectangle` draws a rectangle on image based on passed rectangle coordinates. It uses OpenCV's built in function `cv2.rectangle(img, topLeftPoint, bottomRightPoint, rgbColor, lineWidth)` to draw rectangle. We will use it to draw a rectangle around the face detected in test image.
-# 
-# Second function `draw_text` uses OpenCV's built in function `cv2.putText(img, text, startPoint, font, fontSize, rgbColor, lineWidth)` to draw text on image. 
-# 
-# Now that we have the drawing functions, we just need to call the face recognizer's `predict(face)` method to test our face recognizer on test images. Following function does the prediction for us.
-
-# In[9]:
-
 #this function recognizes the person in image passed
 #and draws a rectangle around detected face with name of the 
 #subject
@@ -235,12 +174,12 @@ def predict(test_img):
     #make a copy of the image as we don't want to chang original image
     img = test_img.copy()
     #detect face from the image
-    face, rect = detect_face(img)
+    face, rect = find_faces(test_img)
 
     #predict the image using our face recognizer 
     label, confidence = face_recognizer.predict(face)
     #get name of respective label returned by face recognizer
-    label_text = subjects[label[0]]
+    label_text = subjects[label]
     
     #draw a rectangle around face detected
     draw_rectangle(img, rect)
@@ -256,29 +195,19 @@ def predict(test_img):
 print("Predicting images...")
 
 #load test images
-test_img1 = cv2.imread("test-data/fig1.jpg")
-test_img2 = cv2.imread("test-data/fig2.jpg")
-test_img3 = cv2.imread("test-data/fig3.jpg")
-test_img4 = cv2.imread("test-data/fig4.jpg")
-test_img5 = cv2.imread("test-data/fig5.jpg")
-test_img6 = cv2.imread("test-data/fig6.jpg")
-test_img7 = cv2.imread("test-data/fig7.jpg")
-
+test_img1 = cv2.imread("test-data/test1.jpg")
+test_img2 = cv2.imread("test-data/test2.jpg")
+test_img3 = cv2.imread("test-data/test3.jpg")
 #perform a prediction
 predicted_img1 = predict(test_img1)
 predicted_img2 = predict(test_img2)
 predicted_img3 = predict(test_img3)
-predicted_img4 = predict(test_img4)
-predicted_img5 = predict(test_img5)
-predicted_img6 = predict(test_img6)
-predicted_img7 = predict(test_img7)
 print("Prediction complete")
 
 #display both images
 cv2.imshow(subjects[1], cv2.resize(predicted_img1, (400, 500)))
 cv2.imshow(subjects[2], cv2.resize(predicted_img2, (400, 500)))
 cv2.imshow(subjects[3], cv2.resize(predicted_img3, (400, 500)))
-cv2.imshow(subjects[4], cv2.resize(predicted_img4, (400, 500)))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 cv2.waitKey(1)
