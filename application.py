dc01f7c3ee10337a63aa0c914c550ec3ef27af56
Richie Ward <33726576+rward2861@users.noreply.github.com>
2018-11-12 12:12:11 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,19 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, session, render_template
 
 application = Flask(__name__)
 
-# add a rule for the index page.
-#application.add_url_rule('/', 'index', (lambda: header_text +
-#    say_hello() + instructions + footer_text))
+#@application.route('/')
+#def profile():
+##  access_token = session.get('access_token')
+#  if access_token is None:
+#  return render_template('profile.html')
+#  else:
+#    return render_template('dashboard.html')
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-#application.add_url_rule('/<username>', 'hello', (lambda username:
-#    header_text + say_hello(username) + home_link + footer_text))
 @application.route('/')
-def profile():
-  return render_template("profile.html")
-# run the app.
+def dashboard():
+  return render_template('dashboard.html')
+
+@application.route('/unlock')
+def unlock():
+    return render_template("unlock.html")
+
+@application.route('/upload')
+def upload():
+    return render_template("upload.html")
+
+@application.route('/account')
+def account():
+    return render_template("account.html")
+'''
+  access_token = session.get('access_token')
+  if access_token is None:
+    return redirect(url_for('profile'))
+  access_token = access_token[0]
+  from urllib2 import Request, urlopen, URLError
+  headers = {'Authorization': 'OAuth ' + access_token}
+  req = Request('https://www.googleapis.com/oauth2/v1/userinfo', None, headers)
+
+  try:
+    res = urlopen(req)
+  except URLError as e: 
+    if e.code == 401:
+      #bad token
+      session.pop('access_token', None)
+      return redirect(url_for('profile'))
+    return res.read()
+  '''
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
